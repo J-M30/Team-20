f70531395bd4d49e7b81fd5fbda4adbf40fa7a41
Tommi Hämäläinen <tommihmli@gmail.com>
2026-02-19 14:06:24 +0200

    Add PHP script for deleting items from database

---
+++ poisto.php
@@ -0,0 +1,37 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+try {
+    $yhteys = mysqli_connect("db", "root", "password", "lounas");
+}
+catch(Exception $e) {
+    echo json_encode(["status" => "error"]);
+    exit;
+}
+
+if (isset($_GET["poistettava"]) && isset($_GET["taulu"])) {
+
+    $poistettava = $_GET["poistettava"];
+    $taulu = $_GET["taulu"];
+
+    if ($taulu == "Ruuat") {
+        $sql = "DELETE FROM Ruuat WHERE RuokaID=?";
+    }
+    elseif ($taulu == "Juomat") {
+        $sql = "DELETE FROM Juomat WHERE JuomaID=?";
+    }
+    elseif ($taulu == "Lisukkeet") {
+        $sql = "DELETE FROM Lisukkeet WHERE LisukeID=?";
+    }
+
+    $stmt = mysqli_prepare($yhteys, $sql);
+    mysqli_stmt_bind_param($stmt, 'i', $poistettava);
+    mysqli_stmt_execute($stmt);
+
+    echo json_encode(["status" => "ok"]);
+}
+
+mysqli_close($yhteys);
+?>
