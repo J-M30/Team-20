eec1f2d0b22843e0807730a93b8f3b8a4785740c
J-M30 <johannamarjoanttila@gmail.com>
2026-02-20 12:57:04 +0200

    Implement PHP backend for adding food items
    Refactor food addition functionality to use PHP for database operations and JSON validation.

--- lisaaRuoka.php
+++ lisaaRuoka.php
@@ -1,90 +1,41 @@
-<!DOCTYPE html>
-<html lang="fi">
-<head>
-    <meta charset="UTF-8">
-    <title>Lounaan muokkaaminen</title>
-</head>
-<body>
-<script>
-    function lahetaRuoka(lomake){
-        var Ruuat=new Object();
-        Ruuat.ruokaNimi=lomake.ruokaNimi.value;
-        Ruuat.ruokaHinta=lomake.ruokaHinta.value;
-        var jsonRuuat=JSON.stringify(Ruuat);
+<?php    
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-        xmlhttp=new XMLHttpRequest();
-        xmlhttp.onreadystatechange=function(){
-            if(this.readyState == 4 && this.status == 200) {
-                document.getElementById("result").innerHTML = this.responseText;            }    
-        };
-        xmlhttp.open("POST", "./lisaaRuoka.php", true);
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send("Ruuat=" + jsonRuuat);
+    try{
+        $yhteys=mysqli_connect("db", "root", "password", "lounas");
+    }
+    catch(Exception $e){
+        exit;
     }
 
-    function lahetaJuoma(lomake2){
-        var Juomat=new Object();
-        Juomat.juomaNimi=lomake2.juomaNimi.value;
-        Juomat.juomaHinta=lomake2.juomaHinta.value;
-        var jsonJuomat=JSON.stringify(Juomat);
+    $json=isset($_POST["Ruuat"]) ? $_POST["Ruuat"] : "";
 
-        xmlhttp=new XMLHttpRequest();
-        xmlhttp.onreadystatechange=function(){
-            if(this.readyState == 4 && this.status == 200) {
-                    document.getElementById("result2").innerHTML = this.responseText;    
-            }    
-        };
-        xmlhttp.open("POST", "./lisaaJuoma.php", true);
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send("Juomat=" + jsonJuomat);
+    if (!($Ruuat=tarkistaJson($json))) {
+        print "Et ole täyttänyt kaikkia kohtia";
+        
     }
 
-    function lahetaLisuke(lomake3){
-        var Lisukkeet=new Object();
-        Lisukkeet.lisukeNimi=lomake3.lisukeNimi.value;
-        Lisukkeet.lisukeHinta=lomake3.lisukeHinta.value;
-        var jsonLisukkeet=JSON.stringify(Lisukkeet);
+    $sql="insert into Ruuat (ruokaNimi, ruokaHinta) values (?, ?)";
+    $stmt=mysqli_prepare($yhteys,$sql);
+    mysqli_stmt_bind_param($stmt, "sd", $Ruuat->ruokaNimi, $Ruuat->ruokaHinta);
+    mysqli_stmt_execute($stmt);
 
-        xmlhttp=new XMLHttpRequest();
-        xmlhttp.onreadystatechange=function(){
-            if(this.readyState == 4 && this.status == 200) {
-                document.getElementById("result3").innerHTML = this.responseText; 
-            }    
-        };
-        xmlhttp.open("POST", "./lisaaLisukkeet.php", true);
-        xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        xmlhttp.send("Lisukkeet=" + jsonLisukkeet);
+    mysqli_close($yhteys);
+
+    print $json;
+
+?>
+
+<?php
+
+   function tarkistaJson($json) {
+    if (empty($json)) {
+        return false;
     }
-
-
-
-</script>
-
-<h2>Lisää ruoka</h2>
-<form id="ruokalomake">
-Nimi:<input type="text" name="ruokaNimi" value=""><br>
-Hinta:<input type="number" name="ruokaHinta" step="0,01" value=""><br>
-<input type="button" name="ok" value="Lähetä" onclick="lahetaRuoka(this.form);"><br>
-<p id="result">Lounaat</p>
-</form>
-
-<h2>Lisää juoma</h2>
-<form id="juomalomake">
-Nimi:<input type="text" name="juomaNimi" value=""><br>
-Hinta:<input type="number" name="juomaHinta" step="0,01" value=""><br>
-<input type="button" name="ok" value="Lähetä" onclick="lahetaJuoma(this.form);"><br>
-<p id="result2">Lounaat</p>
-</form>
-
-<h2>Lisää lisuke</h2>
-<form id="lisukelomake">
-Nimi:<input type="text" name="lisukeNimi" value=""><br>
-Hinta:<input type="number" name="lisukeHinta" step="0,01" value=""><br>
-<input type="button" name="ok" value="Lähetä" onclick="lahetaLisuke(this.form);"><br>
-<p id="result3">Lounaat</p>
-</form>
-
-<a href="menu.html">Takaisin menuun</a>
-
-</body>
-</html>
+    $Ruuat=json_decode($json, false);
+    if (empty($Ruuat->ruokaNimi) || empty($Ruuat->ruokaHinta)) {
+        return false;
+    }
+    return $Ruuat;
+}
+?>
